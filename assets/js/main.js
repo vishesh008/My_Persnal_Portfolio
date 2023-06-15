f19e3e56775cb51e2e154098e62973e558be9c4a
vishesh008 <59003981+vishesh008@users.noreply.github.com>
2023-06-15 19:20:40 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ const navMenu = document.getElementById('nav-menu'),
 navToggle = document.getElementById('nav-toggle'),
 navClose = document.getElementById('nav-close');
 
+
+
 if(navToggle){
     navToggle.addEventListener('click',()=>{
         navMenu.classList.add('show-menu');
@@ -66,3 +68,59 @@ tabs.forEach(tab =>{
    
 
 })
+
+// ============Portfolio==========================
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+
+
+
+
+
+// ===============DARK LIGHT THEME====================
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme)?'dark':'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme)?'uil-moon' : 'uil-sun'
+
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add':'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add':'remove'](iconTheme)
+
+}
+themeButton.addEventListener('click',()=>{
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem('selected-theme' , getCurrentTheme());
+    localStorage.setItem('selected-icon',getCurrentIcon());
+
+})
+
+
+
+
+
+
+
+
+
